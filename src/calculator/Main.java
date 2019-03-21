28a9474c93b8894eefb74fc2c83014d26d397473
8560P\HP <szpilaka@gmail.com>
2019-03-21 22:15:56 +0100

    procedural basic operations

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -2,7 +2,25 @@ package calculator;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello Szimin");
+		System.out.println("Calculator");
+		String input = "- 1 2";
+		String[] splittedInput = input.split(" ");
+		
+		String operation = splittedInput[0];
+		double x = Double.parseDouble(splittedInput[1]);
+		double y = Double.parseDouble(splittedInput[2]);
+		
+		switch(operation) {
+		case "+":
+			System.out.println("Dodawanie");
+			System.out.println(x+y);
+			break;
+		case "-":
+			System.out.println("Odejmowanie");
+			System.out.println(x-y);
+			break;
+		}
+		
 	}
 
 }
